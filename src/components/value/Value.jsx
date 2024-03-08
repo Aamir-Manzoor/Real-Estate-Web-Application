0b1008d0b397ce7e30f5666e5e27d95c4df0f799
aamir221 <aamirmanzoor372@gmail.com>
2024-03-08 22:34:18 +0530

    Accordion values added

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -3,12 +3,14 @@ import {
   AccordionItem,
   AccordionItemPanel,
   AccordionItemHeading,
+  AccordionItemButton,
   AccordionItemState,
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import "./value.css";
 import Main2Img from "../../assets/main3.jpg";
+import data from "../../utils/accordion.js";
 const Value = () => {
   return (
     <section className="v-wrapper">
@@ -23,14 +25,32 @@ const Value = () => {
         {/* rightSide */}
 
         <div className="flexColStart v-right">
-          <span>Our Value</span>
-          <span>value we give to you</span>
-          <span>
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">value we give to you</span>
+          <span className="secondaryText">
             we always help you find the right property
             <br />
             Best Service you can get is from us related to buying the best
             property that will have great value in future.
           </span>
+
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {data.map((item, i) => {
+              return (
+                <AccordionItem key={i} uuid={i} className="accordionItem">
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      <div className="flexCenter icon">{item.icon}</div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         </div>
       </div>
     </section>
