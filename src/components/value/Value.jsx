c258d8882c87fdc8edd61849ca9ec81ec890af8b
aamir221 <aamirmanzoor372@gmail.com>
2024-03-08 08:03:26 +0530

    Added Accordion

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -1,10 +1,15 @@
-
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemPanel,
+  AccordionItemHeading,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import "./value.css";
 const Value = () => {
-  return (
-    <div>
-      Value
-    </div>
-  )
-}
+  return <div>Value</div>;
+};
 
-export default Value
+export default Value;
