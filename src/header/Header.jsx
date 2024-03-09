0b05ece2a453a023be8e8ccf0852de0408e5c3e7
aamir221 <aamirmanzoor372@gmail.com>
2024-03-09 23:50:11 +0530

    adding hamburger button

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,20 +4,23 @@ import { BiMenuAltRight } from "react-icons/bi";
 import "./Header.css";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const getMenuStyles = () => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpen && "-100%" };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src={HeroImg} alt="Hero-image" width={100} />
-        <div className="h-menu flexCenter"
-        style={getMenuStyles(menuOpen)}
-        >
+        <div className="h-menu flexCenter" style={getMenuStyles(menuOpen)}>
           <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
           <a href="">GetStarted</a>
           <button className="button">Contact</button>
         </div>
-        <div className="menu-icon" onClick={()=> setMenuOpen((prev) => !prev)}>
+        <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
           <BiMenuAltRight size={30} />
         </div>
       </div>
