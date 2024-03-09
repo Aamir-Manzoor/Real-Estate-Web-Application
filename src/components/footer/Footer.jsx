3840133d2463446b61df30dee8ebd7cc303a8d28
aamir221 <aamirmanzoor372@gmail.com>
2024-03-09 22:46:19 +0530

    left side footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,20 @@
+import Imgfooter from "../../assets/logo.jpg";
 const Footer = () => {
   return (
-    <section>
-        
-      
-    </section>
-  )
-}
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* leftSide */}
+        <div className="flexColStart f-left">
+          <img src={Imgfooter} alt="Image" width={120} />
+          <span className="secondaryText">
+            Our Vision is to help by providing to the best properties
+          </span>
+        </div>
 
-export default Footer
+        {/* right Side  */}
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
