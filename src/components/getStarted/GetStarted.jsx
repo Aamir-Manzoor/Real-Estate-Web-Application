500511041a27f0d4dc31e6351cd50367c4206ffa
aamir221 <aamirmanzoor372@gmail.com>
2024-03-09 22:36:58 +0530

    Completed getStarted

--- src/components/getStarted/GetStarted.jsx
+++ src/components/getStarted/GetStarted.jsx
@@ -1,14 +1,23 @@
-
+import './getStarted.css'
 const GetStarted = () => {
   return (
     <section>
       <div className="paddings innerWidth g-container">
         <div className="flexColCenter inner-container">
-            <span>Get Started With Real Estate</span><span></span><span></span>
+          <span className="primaryText">Get Started With Real Estate</span>
+          <span className="secondaryText">
+            Subscribe and find teh attractive quotes from us
+            <br />
+            Find your property soon
+          </span>
+
+          <button className='button'>
+            <a href="mailto:dummyaamir89@gmail.com" className='link'>Get Started</a>
+          </button>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default GetStarted
+export default GetStarted;
